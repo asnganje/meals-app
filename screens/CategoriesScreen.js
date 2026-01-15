c30e8456e15af38e82b6ece418052859a35aca3c
asnganje <abd.nganje@gmail.com>
2026-01-15 18:52:28 +0300

    add basic categories screen with dummy data

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,19 @@
+import { FlatList } from "react-native";
+import {CATEGORIES} from "../data/dummy-data"
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem(itemData) {
+  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
+}
+
+function CategoriesScreen() {
+  return(
+    <FlatList
+      data={CATEGORIES}
+      renderItem={(itemData)=>renderCategoryItem(itemData)}
+      keyExtractor={(item)=>item.id}
+    />
+  )  
+}
+
+export default CategoriesScreen;
