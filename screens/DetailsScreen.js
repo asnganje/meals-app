376f0b038ce0c1bde174a58b4cb866f74e466ab6
asnganje <abd.nganje@gmail.com>
2026-01-16 20:30:26 +0300

    Add star icon on detail screen

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,12 +1,27 @@
-import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView, Button } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetail";
 import SubTitle from "../components/MealDetail/SubTitle";
 import List from "../components/MealDetail/List";
+import { useLayoutEffect } from "react";
+import IconButton from "../components/IconButton";
 
-function DetailsScreen({route}) {
-  const mId = route.params.mealId
-  const selectedMeal = MEALS.find((meal)=>meal.id === mId)  
+function DetailsScreen({route, navigation}) {
+  const mId = route.params.mealId;
+  const selectedMeal = MEALS.find((meal)=>meal.id === mId);
+
+  function headerButtonPressHandler() {
+    console.log("tapped");
+    
+  }
+
+  useLayoutEffect(()=> {
+    navigation.setOptions({
+      headerRight: ()=> {
+        return <IconButton icon="star" color="white" onPress={headerButtonPressHandler}/>
+      }
+    })
+  }, [navigation, headerButtonPressHandler])
   
   return(
     <ScrollView style={styles.rootContainer}>
